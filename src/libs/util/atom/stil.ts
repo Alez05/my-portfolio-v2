7e6aea096b0be9599a9cbd62b7b09d65b58f2e81
alez05 <cercel.alexandru436@gmail.com>
2024-08-09 23:36:21 +0100

    flexbox - update features Fixes #5

--- src/libs/util/atom/stil.ts
+++ src/libs/util/atom/stil.ts
@@ -1,4 +1,9 @@
-export const getStil = (obiect: Record<string, string | number>) => {
+interface stilOptions {
+  borderRadiusUnit?: 'px' | '%';
+  unit?: 'px' | 'rem';
+}
+
+export const getStil = (obiect: Record<string, string | number | number[]>) => {
   const paddingList = (
     Array.isArray(obiect.padding) ? obiect.padding : [obiect.padding]
   ) as number[];
@@ -24,6 +29,43 @@ export const getStil = (obiect: Record<string, string | number>) => {
 
   const position = obiect.position ? `${obiect.position}` : '';
 
+  const gapList = (
+    Array.isArray(obiect.gap) ? obiect.gap : [obiect.gap]
+  ) as number[];
+
+  const gap = obiect.gap
+    ? gapList
+        .slice(0, 4)
+        .map((n: number) => `${n / 16}rem`)
+        .join(' ')
+    : '';
+
+  const width = obiect.width ? `${obiect.width}px` : '';
+
+  const height = obiect.height ? `${obiect.height}px` : '';
+
+  const flexDirection = obiect.flexDirection ? `${obiect.flexDirection}` : '';
+
+  const flexWrap = obiect.flexWrap ? `${obiect.flexWrap}` : '';
+
+  const borderRadius = () => {
+    const unit = obiect.borderRadiusUnit || 'px';
+    if (obiect.borderRadius) {
+      if (typeof obiect.borderRadius === 'string') {
+        return obiect.borderRadius;
+      }
+      if (Array.isArray(obiect.borderRadius)) {
+        return (obiect.borderRadius as number[])
+          .map((value) => `${value}${unit}`)
+          .join(' ');
+      }
+      if (typeof obiect.borderRadius === 'number') {
+        return `${obiect.borderRadius}${unit}`;
+      }
+    }
+    return '';
+  };
+
   return {
     '--padding': padding,
     '--font-size': fontSize,
@@ -32,5 +74,11 @@ export const getStil = (obiect: Record<string, string | number>) => {
     '--justify-content': justifyContent,
     '--align-content': alignContent,
     '--position': position,
+    '--gap': gap,
+    '--width': width,
+    '--height': height,
+    'flex-direction': flexDirection,
+    'flex-wrap': flexWrap,
+    'border-radius': borderRadius(),
   };
 };

b5a2cecd442e33953e9f26eacd87d7cf8b0baaf2
alez05 <cercel.alexandru436@gmail.com>
2024-08-28 12:57:26 +0100

    create Icon as an atom component Fixes #23

---
+++ src/libs/ui/atom/icon/icon.type.ts
@@ -0,0 +1,9 @@
+export interface TIcon extends React.HTMLAttributes<HTMLSpanElement> {
+  icon?: string;
+  backgroundColor?: string;
+  fontSize?: number;
+  width?: string;
+  height?: string;
+  borderRadius?: string;
+  round?: boolean;
+}
--- src/libs/ui/atom/index.ts
+++ src/libs/ui/atom/index.ts
@@ -1,5 +1,6 @@
 export { default as Button } from './button/button';
 export { default as FlexBox } from './flexbox/flexbox';
 export { default as GridBox } from './gridbox/gridbox';
+export { default as Icon } from './icon/icon';
 export { default as Input } from './input/input';
 export { default as Text } from './text/text';

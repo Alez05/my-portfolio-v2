eff5aadddb9b4c07cd0a8b04302ce6411557abfc
alez05 <cercel.alexandru436@gmail.com>
2024-08-31 14:21:04 +0100

    navigation implemented Fixes #27

--- src/libs/ui/index.ts
+++ src/libs/ui/index.ts
@@ -1,6 +1,10 @@
+// atom components
 export { default as Button } from './atom/button/button';
 export { default as FlexBox } from './atom/flexbox/flexbox';
 export { default as GridBox } from './atom/gridbox/gridbox';
 export { default as Icon } from './atom/icon/icon';
 export { default as Input } from './atom/input/input';
 export { default as Text } from './atom/text/text';
+
+// organism components
+export { default as NavBar } from './organism/nav/nav';

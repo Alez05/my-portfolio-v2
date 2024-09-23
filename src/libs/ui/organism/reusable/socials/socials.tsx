b2fa462a85beff073b516f785fd9f4ee928feee2
alez05 <cercel.alexandru436@gmail.com>
2024-09-23 11:48:13 +0100

    update socialComponent Fixes #84

--- src/libs/ui/organism/reusable/socials/socials.tsx
+++ src/libs/ui/organism/reusable/socials/socials.tsx
@@ -1,49 +1,34 @@
 import './socials.css';
 import { FlexBox, Icon } from '../../../atom';
+import { IconLink } from '@/libs/ui/molecule';
 
 const Socials = () => {
   return (
-    <FlexBox
-      flexDirection='row'
-      gap={20}
-      width='300px'
-    >
-      <a
-        target='_blank'
-        rel='noopener noreferrer'
-        href='https://github.com/Alez05'
-      >
-        <Icon
-          icon='github'
-          width='50px'
-          height='50px'
-          fontSize={30}
-        />
-      </a>
-      <a
-        target='_blank'
-        rel='noopener noreferrer'
-        href='https://www.linkedin.com/in/alexandru-cercel-6b977b220/'
-      >
-        <Icon
-          icon='linkedin'
-          width='50px'
-          height='50px'
-          fontSize={30}
-        />
-      </a>
-      <a
-        target='_blank'
-        rel='noopener noreferrer'
-        href='mailto:cercel.alexandru436@gmail.com'
-      >
-        <Icon
-          icon='envelope'
-          width='50px'
-          height='50px'
-          fontSize={30}
-        />
-      </a>
+    <FlexBox flexDirection='row'>
+      <IconLink
+        altText='github'
+        icon='github'
+        label='Github'
+        url='https://github.com/Alez05'
+      />
+      <IconLink
+        altText='email'
+        icon='envelope'
+        label='email'
+        url='mailto:cercel.alexandru436@gmail.com'
+      />
+      <IconLink
+        altText='message'
+        icon='messagec'
+        label='message'
+        url='https://github.com/Alez05'
+      />
+      <IconLink
+        altText='linkedin'
+        icon='linkedin'
+        label='linkedin'
+        url='https://github.com/Alez05'
+      />
     </FlexBox>
   );
 };

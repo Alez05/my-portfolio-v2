a9efd973383f7223f170f0080f313ef78837a941
alez05 <cercel.alexandru436@gmail.com>
2024-10-30 22:11:20 +0000

    create link group component and test it out Fixes #113

---
+++ src/libs/type/lib/link.ts
@@ -0,0 +1,20 @@
+export type TLink = {
+  href: string
+  children: React.ReactNode
+  target?: string
+  rel?: string
+  title?: string
+  label?: string
+  onClick?: () => void
+  // costum
+  color?: string
+  border?: string
+  borderRadius?: string
+  gap?: number | number[]
+  padding?: number | number[]
+  fontSize?: number
+  width?: string
+  height?: string
+  cursor?: string
+  boxShadow?: string
+}
